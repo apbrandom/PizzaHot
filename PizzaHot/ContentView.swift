59df2cdbfe88dab766b642c58856f5cb866f7c11
apbrandom <apbrandom@gmail.com>
2023-10-16 18:48:51 +0200

    Autorization screen layout

--- PizzaHot/ContentView.swift
+++ PizzaHot/ContentView.swift
@@ -8,14 +8,62 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var email = ""
+    @State var password = ""
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Authorization")
+                .padding()
+                .padding(.horizontal, 30)
+                .font(.title2.bold())
+                .background(.alphaWhite)
+                .foregroundStyle(.darkBrown)
+                .clipShape(.capsule)
+                .padding(.vertical, 40)
+            
+            VStack(spacing: 10) {
+                TextField("Enter email", text: $email)
+                    .padding()
+                    .background(.alphaWhite)
+                    .clipShape(.buttonBorder)
+                    .padding()
+                
+                SecureField("Enter password", text: $password)
+                    .padding()
+                    .background(.alphaWhite)
+                    .clipShape(.buttonBorder)
+                    .padding([.horizontal, .bottom])
+                
+                Button("Login") {
+                    print("Login pressed")
+                    
+                }
+                .padding()
+                .frame(maxWidth: .infinity)
+                .background(LinearGradient(colors: [.yellow, .orange], startPoint: .leading, endPoint: .trailing))
+                .font(.title3.bold())
+                .foregroundStyle(.darkBrown)
+                .clipShape(.buttonBorder)
+                .padding()
+                
+                Button("Not with us yet?") {
+                    print("register button pressed")
+                }
+                .padding()
+            }
+            .background(.alphaWhite)
+            .padding()
+            .clipShape(.buttonBorder)
         }
-        .padding()
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(
+            Image("backgroundImage")
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+                .edgesIgnoringSafeArea(.all)
+        )
     }
 }
 
