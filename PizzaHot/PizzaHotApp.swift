86f5caef384f32faaa45decadbbd08a0cabe344c
apbrandom <apbrandom@gmail.com>
2023-10-16 16:50:21 +0200

    Initial Commit

---
+++ PizzaHot/PizzaHotApp.swift
@@ -0,0 +1,17 @@
+//
+//  PizzaHotApp.swift
+//  PizzaHot
+//
+//  Created by Vadim Vinogradov on 16.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct PizzaHotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
